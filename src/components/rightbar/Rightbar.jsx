341957fdc0254056be453c9f6d5a4a5ba88e6965
Sai Tejas A R <saitejaspallavi@gmail.com>
2023-05-03 23:13:51 +0530

    feat: added rightbar components

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,13 @@ import "./rightbar.css"
 export default function Rightbar(){
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="assets/party.jpg" alt="" />
+                    <span className="birthdayText"><b>Anish Krishna</b> and <b>2 other friends</b> have a birthday today</span>
+                </div>
+                <img className="rightbarAd" src="assets/posts/12.jpg" alt="" />
+            </div>
         </div>
     )
 }
