43b2005a9e3990f55e40e409b82b56e9e1830820
Sai Tejas A R <saitejaspallavi@gmail.com>
2023-05-03 20:53:10 +0530

    feat: share component adding multiple options

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -6,13 +6,28 @@ export default function Share() {
     <div className="share">
         <div className="shareWrapper">
           <div className="shareTop">
-            <img className="shareProfileImg" src="/assets/person-pfp/4.png" alt="" />
+          <img className="shareProfileImg" src="/assets/person-pfp/4.png" alt="" />
             <input placeholder="What's in your mind Sai?" 
             className="shareInput" />
           </div>
           <hr className="shareHr"/>
           <div className="shareBottom">
             <div className="shareOptions">
+              <div className="shareOption">
+                <PermMedia className="shareIcon"/>
+                <span className="shareOptionText">Photo/Video</span>
+              </div>
+
+              <div className="shareOption">
+                <PermMedia className="shareIcon"/>
+                <span className="shareOptionText">Photo/Video</span>
+              </div>
+
+              <div className="shareOption">
+                <PermMedia className="shareIcon"/>
+                <span className="shareOptionText">Photo/Video</span>
+              </div>
+
               <div className="shareOption">
                 <PermMedia className="shareIcon"/>
                 <span className="shareOptionText">Photo/Video</span>
