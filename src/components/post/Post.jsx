bfc115b6d0a85dc1c16ee757d99b6254edcbfa7e
Sai Tejas A R <saitejaspallavi@gmail.com>
2023-05-03 21:58:26 +0530

    feat: created top head part of the post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,9 +1,24 @@
 import "./post.css"
+import {MoreVert} from "@mui/icons-material"
 
 export default function Post() {
   return (
-    <div>
-        hello mf
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img className="postProfileImg" src="/assets/person-pfp/4.png" alt="" />
+                    <span className="postUsername">Sai Tejas A R</span>
+                    <span className="postDate">5 mins ago</span>
+                </div>
+                <div className="postTopRight">
+                    <MoreVert/>
+
+                </div>
+            </div>
+            <div className="postCenter"></div>
+            <div className="postBottom"></div>
+        </div>
     </div>
   )
 }
