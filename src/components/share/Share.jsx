58a75d6be5d907bdf8d4c82176229775f20ec4bc
Sai Tejas A R <saitejaspallavi@gmail.com>
2023-05-03 20:32:02 +0530

    feat: making sharing component

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,25 @@
+import "./share.css"
+import {PermMedia} from "@mui/icons-material"
+
+export default function Share() {
+  return (
+    <div className="share">
+        <div className="shareWrapper">
+          <div className="shareTop">
+            <img className="shareProfileImg" src="/assets/person-pfp/4.png" alt="" />
+            <input placeholder="What's in your mind Sai?" 
+            className="shareInput" />
+          </div>
+          <hr className="shareHr"/>
+          <div className="shareBottom">
+            <div className="shareOptions">
+              <div className="shareOption">
+                <PermMedia className="shareIcon"/>
+                <span className="shareOptionText">Photo/Video</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+  )
+}
