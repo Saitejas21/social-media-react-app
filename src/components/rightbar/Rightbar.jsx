72728d475318b9089a8b31278d7f9d3ccdd2a0cb
Sai Tejas A R <saitejaspallavi@gmail.com>
2023-05-03 23:21:15 +0530

    feat: adding online friends in rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -9,6 +9,15 @@ export default function Rightbar(){
                     <span className="birthdayText"><b>Anish Krishna</b> and <b>2 other friends</b> have a birthday today</span>
                 </div>
                 <img className="rightbarAd" src="assets/posts/12.jpg" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightBarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="assets/person-pfp/4.png" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     )
